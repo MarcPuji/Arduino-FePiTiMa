1cabf285e12bc1f0b543e51a25f3f7f407656b06
kareyan <tigran.kareyan@gmail.com>
2016-02-27 16:17:29 +0100

    Added files via upload

---
+++ Practica2_Semafor.ino
@@ -0,0 +1,40 @@
+int interuptor=0;
+int pininter=2;
+int ledvermell=13;
+int ledgroc=12;
+int ledverd=11;
+int temps=2000;
+void setup() 
+{
+  pinMode(ledvermell,OUTPUT);
+  pinMode(ledgroc,OUTPUT);
+  pinMode(ledverd,OUTPUT);
+  pinMode(pininter,INPUT);
+}
+
+void loop() 
+  {
+  interuptor=digitalRead(pininter);
+  if (interuptor == LOW)
+  {
+    digitalWrite(ledvermell, LOW);
+    digitalWrite(ledgroc, LOW);
+    digitalWrite(ledverd, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledvermell,LOW);
+    digitalWrite(ledverd,LOW);
+    digitalWrite(ledgroc,HIGH);
+    delay(temps);
+    digitalWrite(ledvermell,HIGH);
+    digitalWrite(ledverd,LOW);
+    digitalWrite(ledgroc,LOW);
+    delay(temps);
+    digitalWrite(ledvermell, LOW);
+    digitalWrite(ledgroc, LOW);
+    digitalWrite(ledverd, HIGH);
+    delay(temps);
+  }
+}
+
