58ce75c3cd308375eeff60d3e337c27a1ccc258c
MarcPuji <marcpuru@gmail.com>
2016-02-29 16:32:25 +0100

    Update P8_RelojdeCocina.ino

--- P8_RelojdeCocina.ino
+++ P8_RelojdeCocina.ino
@@ -2,7 +2,7 @@ unsigned long tprev = 0;
 int esint = 0;
 int esintprev = 0;
 int led = 2;
-unsigned long tint = 25000;
+long tint = 25000;
 
 void setup() {
   for(int x = 2;x < 7;x++){
@@ -20,21 +20,11 @@ void loop() {
     led++;
     if(led == 6){
       tone(6,254,100);
-      while(esint == esintprev){
-        for(int x = 2;x<6;x++){
-          digitalWrite(x,LOW);
-        }
-        delay(500);
-        for(int x = 2;x<6;x++){
-          digitalWrite(x,HIGH);
-        }
-        esint = digitalRead(8);
-      } 
     }
   }
   esint = digitalRead(8);
   if(esint != esintprev){
-    for(int x = 2;x<6;x++){
+    for(int x = 2;x < 6;x++){
       digitalWrite(x,LOW);
     }
     led = 2;
