db90b0aed086f379ebec0adcb57e8c68dff0e572
kareyan <tigran.kareyan@gmail.com>
2016-03-02 15:15:30 +0100

    Update P2_Semafor.ino

--- P2_Semafor.ino
+++ P2_Semafor.ino
@@ -3,12 +3,14 @@ int pininter=2;
 int ledvermell=13;
 int ledgroc=12;
 int ledverd=11;
+int ledvermellvianen=10
 int temps=2000;
 void setup() 
 {
   pinMode(ledvermell,OUTPUT);
   pinMode(ledgroc,OUTPUT);
   pinMode(ledverd,OUTPUT);
+  pinMode(ledvermellvianen,OUTPUT);
   pinMode(pininter,INPUT);
 }
 
@@ -20,20 +22,24 @@ void loop()
     digitalWrite(ledvermell, LOW);
     digitalWrite(ledgroc, LOW);
     digitalWrite(ledverd, HIGH);
+    digitalWrite(ledvermellvianen, HIGH);
   }
   else
   {
     digitalWrite(ledvermell,LOW);
     digitalWrite(ledverd,LOW);
     digitalWrite(ledgroc,HIGH);
+    digitalWrite(ledvermellvianen, HIGH);
     delay(temps);
     digitalWrite(ledvermell,HIGH);
     digitalWrite(ledverd,LOW);
     digitalWrite(ledgroc,LOW);
+    digitalWrite(ledvermellvianen, LOW);
     delay(temps);
     digitalWrite(ledvermell, LOW);
     digitalWrite(ledgroc, LOW);
     digitalWrite(ledverd, HIGH);
+    digitalWrite(ledvermellvianen, LOW);
     delay(temps);
   }
 }
