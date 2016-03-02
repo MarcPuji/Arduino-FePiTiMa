1fe6c974c8c5862a38ee8710940ee5918f85feff
MarcPuji <marcpuru@gmail.com>
2016-03-02 14:46:33 +0100

    Added files via upload

---
+++ P6_Thereminllum.ino
@@ -0,0 +1,33 @@
+int Valordelsensor;
+int Valorminsensor=1023;
+int Valormaxsensor=0;
+
+const int Pinled=13;
+
+void setup() {
+  pinMode(Pinled,13);
+  digitalWrite(Pinled,HIGH);
+
+  while(millis()<5000){
+    Valodelsensor=analogRead(A0);
+    if(valorsensor>Valormaxsensor){
+      Valormaxsensor=valorsensor;
+      }
+      if(Valorsensor < valorminsensor){
+        Valormimsensor=valorsensor;
+        }
+  }
+  digitalWrite(Pinled,LOW);
+  
+}
+
+void loop() {
+  Valorsensensor=analogRead(A0);
+
+  int tonalitat=map(valorsensor,valorminsensor,valormaxsensor,50,4000);
+
+  tone(8,tonalitat,20);
+
+  delay(10);
+  
+}
